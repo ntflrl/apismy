d27f0c1369caa7d7bd66ff7e0b0a6ab524a68028
ntflrl <catfallflat@gmail.com>
2022-11-20 12:13:35 +0300

    Release

---
+++ api.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+import discordwebhook
+app = Flask(__name__)
+hook = discordwebhook.Webhook(url='https://discord.com/api/webhooks/1043796010834608178/zLMVOVYIq_K6cumFn51HZKMDDLGIceuShbHgINd6H_nQovHOHP8Bc6VQkNp9tCgXEI3b')
+@app.route('/api/v1.0/reloadState/<string:State>', methods=['GET'])
+def state(State):
+    hook.send_sync(content=State)
+    return jsonify({"newState": State})
+if __name__ == "__main__":
+    app.run(debug=False)
