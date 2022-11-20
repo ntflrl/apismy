f6e41cc6f55cd22a0d5f7057e20fbf7c965a4c76
ntflrl <100021997+ntflrl@users.noreply.github.com>
2022-11-20 12:42:19 +0300

    bugfix #2

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import discordwebhook
+import os
 app = Flask(__name__)
 hook = discordwebhook.Webhook(url='https://discord.com/api/webhooks/1043796010834608178/zLMVOVYIq_K6cumFn51HZKMDDLGIceuShbHgINd6H_nQovHOHP8Bc6VQkNp9tCgXEI3b')
 @app.route('/api/v1.0/reloadState/<string:State>', methods=['GET'])
